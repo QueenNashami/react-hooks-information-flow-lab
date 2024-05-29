233c88638164808ee010ceb0a9547966e55c5cdc
Brigid Wanza <wanzanashami@gmail.com>
2024-05-29 11:37:17 +0300

    Passes all the tests

---
+++ src/components/Filter.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Filter({ onCategoryChange }) {
+  const handleChange = (event) => {
+    onCategoryChange(event.target.value);
+  };
+
+  return (
+    <div className="Filter">
+      <select
+        name="filter"
+        role="combobox"
+        onChange={handleChange}
+      >
+        <option value="All">Filter by category</option>
+        <option value="Produce">Produce</option>
+        <option value="Dairy">Dairy</option>
+        <option value="Dessert">Dessert</option>
+      </select>
+    </div>
+  );
+}
+
+export default Filter;
